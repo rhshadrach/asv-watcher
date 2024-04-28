ae69c767bba57a87806011ff6958143781c7570e
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2024-04-28 08:17:01 -0400

    Cleanup Watcher (#18)

---
+++ tests/test_watcher.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+from asv_watcher import Watcher
+
+
+def test_commit_range():
+    watcher = Watcher.__new__(Watcher)
+    watcher._data = pd.DataFrame(
+        {
+            "name": "benchmark",
+            "params": "",
+            "revision": [0, 1, 2],
+            "git_hash": ["a0", "a1", "a2"],
+            "is_regression": [True, False, True],
+        }
+    ).set_index(["name", "params", "revision"])
+    watcher._regressions = watcher._data[watcher._data.is_regression]
+    expected = "a1...a2"
+    result = watcher.commit_range(git_hash="a2")
+    assert result == expected, f"{result=} vs {expected=}"
